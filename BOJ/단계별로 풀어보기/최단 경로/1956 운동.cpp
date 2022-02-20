8823311bf21eabf46b901b9838726b80c08275d9
ahnsanghyeok <48887925+jooa7878@users.noreply.github.com>
2022-02-20 13:29:22 +0900

    Update 1956 운동.cpp

--- BOJ/단계별로 풀어보기/최단 경로/1956 운동.cpp
+++ BOJ/단계별로 풀어보기/최단 경로/1956 운동.cpp
@@ -9,47 +9,57 @@ using namespace std;
 
 #define endl '\n'
 
-const int INF = 1600001;
+const int INF = 987654321;
 int V, E, answer = INF;
 int arr[401][401];
 
 void solution()
 {
-	for (int k = 1; k <= V; k++)
-	{
-		for (int i = 1; i <= V; i++)
-		{
-			for (int j = 1; j <= V; j++)
-			{
-				if (arr[i][j] > arr[i][k] + arr[k][j]) arr[i][j] = arr[i][k] + arr[k][j];
-			}
-		}
-	}
+    for (int k = 1; k <= V; k++)
+    {
+        for (int i = 1; i <= V; i++)
+        {
+            for (int j = 1; j <= V; j++)
+            {
+                if (arr[i][j] > arr[i][k] + arr[k][j])
+                    arr[i][j] = arr[i][k] + arr[k][j];
+            }
+        }
+    }
 
-	for (int i = 1; i <= V; i++)
-	{
-		if (answer > arr[i][i]) answer = arr[i][i];
-	}
-	if (answer == INF) cout << -1 << endl;
-	else cout << answer << endl;
+    for (int i = 1; i <= V; i++)
+    {
+        if (answer > arr[i][i])
+            answer = arr[i][i];
+    }
+    if (answer == INF)
+        cout << -1 << endl;
+    else
+        cout << answer << endl;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	std::cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
 
-	cin >> V >> E;
+    cin >> V >> E;
 
-	memset(arr, INF, sizeof(arr));
+    for (int i = 1; i <= V; i++)
+    {
+        for (int j = 1; j <= V; j++)
+        {
+            arr[i][j] = INF;
+        }
+    }
 
-	for (int i = 0; i < E; i++)
-	{
-		int from, to, weight;
-		cin >> from >> to >> weight;
-		arr[from][to] = weight;
-	}
-	
-	solution();
+    for (int i = 0; i < E; i++)
+    {
+        int from, to, weight;
+        cin >> from >> to >> weight;
+        arr[from][to] = weight;
+    }
+
+    solution();
 }
