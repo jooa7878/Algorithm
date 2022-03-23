9868feb3d5788783750c38fd794f58a9381912bb
ahnsanghyeok <48887925+jooa7878@users.noreply.github.com>
2022-03-23 23:12:50 +0900

    Create 1967 트리의 지름.cpp

---
+++ BOJ/단계별로 풀어보기/트리/1967 트리의 지름.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define endl '\n';
+#define ll long long;
+
+using namespace std;
+
+int N, answer, furthest_node;
+vector<pair<int, int> > adj[100001];
+bool visited[100001];
+
+void solution(int node)
+{
+  int res = 0;
+  queue<pair<int, int> > q;
+
+  q.push({0, node});
+  visited[node] = true;
+  while (!q.empty())
+  {
+    int distance = q.front().first;
+    int node = q.front().second;
+
+    if (distance > res)
+    {
+      res = distance;
+      furthest_node = node;
+    }
+
+    q.pop();
+
+    for (int i = 0; i < adj[node].size(); i++)
+    {
+      auto item = adj[node][i];
+      int nDistance = item.first;
+      int nNode = item.second;
+
+      if (!visited[nNode])
+      {
+        visited[nNode] = true;
+        q.push({distance + nDistance, nNode});
+      }
+    }
+  }
+
+  if (res > answer)
+    answer = res;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cout.tie(NULL);
+  cin.tie(NULL);
+
+  cin >> N;
+
+  for (int i = 0; i < N - 1; i++)
+  {
+    int from, to, weight;
+
+    cin >> from >> to >> weight;
+
+    adj[from].push_back({weight, to});
+    adj[to].push_back({weight, from});
+  }
+
+  solution(1);
+  for (int j = 1; j <= N; j++)
+    visited[j] = false;
+  solution(furthest_node);
+  cout << answer << endl;
+}
